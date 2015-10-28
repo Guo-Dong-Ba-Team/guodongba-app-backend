80666f0f681560e5279385b64fa594ac440a1906
Yunfeng Wang <wangyf11@mail.ustc.edu.cn>
2015-10-28 22:30:52 +0800

    Update OrderServlet

---
+++ src/server/GymDetailServlet.java
@@ -0,0 +1,120 @@
+package server;
+
+import util.DatabaseUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by ustc on 2015/10/27.
+ */
+@WebServlet(name = "GymDetailServlet")
+public class GymDetailServlet extends HttpServlet
+{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        String gym_id = request.getParameter("gym_id");
+       // response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json, charset=UTF-8");
+
+        PrintWriter out = response.getWriter();
+        out.println("{");
+        out.println("\"gym_detail\": ");
+
+        String name = null;
+        String longtitude = null;
+        String latitude = null;
+        String man_image = null;
+        String single_price = null;
+        String vip_price = null;
+        String discount = null;
+        String addres_city = null;
+        String addres_detail = null;
+        String phone = null;
+        String open_time = null;
+        String hardware = null;
+        String service = null;
+        String star_level = null;
+
+
+        ResultSet resultSet = null;
+
+        try
+        {
+            if (gym_id != null)//请求格式正确
+            {
+                resultSet = DatabaseUtil.getGymDetail(gym_id);
+                while (resultSet.next())
+                {
+                    name = resultSet.getString(1);
+                    longtitude = String.valueOf(resultSet.getFloat(2));
+                    latitude = String.valueOf(resultSet.getFloat(3));
+                    man_image = resultSet.getString(4);
+
+                    //从数据库中请求场馆详情图片
+                    String detail_images = "";
+                    ResultSet resultSet1 = DatabaseUtil.getGymImages(gym_id);
+                    while (resultSet1.next())
+                    {
+                        detail_images += "\"" + resultSet1.getString(1) + "\",";
+                    }
+
+                    single_price = String.valueOf(resultSet.getInt(5));
+                    vip_price = String.valueOf(resultSet.getInt(6));
+                    discount = String.valueOf(resultSet.getFloat(7));
+                    addres_city = resultSet.getString(8);
+                    addres_detail = resultSet.getString(9);
+                    phone = resultSet.getString(10);
+                    open_time = resultSet.getString(11);
+                    hardware = resultSet.getString(12);
+                    service = resultSet.getString(13);
+
+                    //读取商家星级
+                    star_level = String.valueOf(DatabaseUtil.getGymStarLevel(gym_id));
+
+                    String everyGym = "{\"name\": \"" + name +
+                            "\", \"longtitude\": " + longtitude +
+                            ", \"latitude\": " + latitude +
+                            ", \"main_image\": \"" + man_image +
+                            "\", \"detail_images\":[ " + detail_images +
+                            "], \"single_price\": " + single_price +
+                            ", \"vip_price\": " + vip_price+
+                            ", \"discount\": " + discount +
+                            ", \"address_city\": \"" + addres_city +
+                            "\", \"address_detail\": \"" + addres_detail +
+                            "\", \"phone_num\": \"" + phone +
+                            "\", \"open_time\": \"" + open_time +
+                            "\", \"hardware\": \"" + hardware +
+                            "\", \"service\": \"" + service +
+                            "\", \"star_level\": " + star_level +
+                            "}";
+
+                    out.println(everyGym);
+                }
+            }
+            out.flush();
+
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+
+        }
+
+        out.println("}");
+    }
+}
---
+++ src/util/DatabaseUtil.java
@@ -0,0 +1,176 @@
+package util;
+
+import javax.swing.*;
+import java.sql.*;
+
+/**
+ * Created by ustc on 2015/10/26.
+ */
+public class DatabaseUtil
+{
+    private static Connection connection = null;
+
+    public static Connection getConnect(String host, String database, String user, String password)
+            throws ClassNotFoundException, SQLException
+    {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://" + host + "/" + database, user, password);
+//        return DriverManager.getConnection("jdbc:mysql://localhost/guodongba", "root", "");
+    }
+
+    public static void initConnection()
+    {
+        if (connection == null)
+        {
+            try
+            {
+                connection = getConnect("localhost", "guodongba", "root", "");
+
+            } catch (ClassNotFoundException e)
+            {
+                e.printStackTrace();
+            } catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static int customerRegister(String registName, String password, String registerPhone)
+            throws SQLException
+    {
+        final int FLAG_PHONE_HAS_REGISTERED = 1;
+        final int FLAG_USERNAME_HAS_REGISTERED = 2;
+
+        initConnection();
+        Statement statement = connection.createStatement();
+
+        //ÿ��ע��ʱ���ȼ����ֻ����Ƿ��Ѿ�ע��
+        String s1 = "SELECT phone FROM customer_info WHERE phone = \'" + registerPhone + "\';";
+        ResultSet resultSet = statement.executeQuery(s1);
+        if (resultSet.next())
+        {
+            return FLAG_PHONE_HAS_REGISTERED;
+        }
+
+        //ÿ��ע��ʱ���ȼ���û����Ƿ��Ѿ�ע��
+        String s2 = "SELECT username FROM customer_info WHERE username = \'" + registName + "\';";
+        ResultSet resultSet2 = statement.executeQuery(s2);
+        if (resultSet2.next())
+        {
+            return FLAG_USERNAME_HAS_REGISTERED;
+        }
+
+        String sentence = "INSERT INTO customer_info" + " VALUES ( \'" + registName + "\', \'" + password + "\',\'" + registerPhone + "\',1);";
+        int result = statement.executeUpdate(sentence); //�û�ע��ʱ��Ĭ���ǵ�¼��
+
+        statement.close();
+        return 0;
+    }
+
+    public static int customerLogin(String phone, String password)
+            throws SQLException
+    {
+        final int FLAG_PHONE_NOT_EXIST = 1;
+        final int FLAG_PHONE_PASSWORD_WRONG = 2;
+
+        initConnection();
+        Statement statement = connection.createStatement();
+
+        String sentence1 = "SELECT phone FROM customer_info WHERE phone = \'" + phone + "\';";
+        ResultSet resultSet1 = statement.executeQuery(sentence1);
+        if (!resultSet1.next())
+        {
+            return FLAG_PHONE_NOT_EXIST;
+        }
+
+        String sentence2 = "SELECT phone, password FROM customer_info WHERE phone = \'" + phone + "\' and password = \'" + password + "\';";
+        ResultSet resultSet2 = statement.executeQuery(sentence2);
+        if (!resultSet2.next())
+        {
+            return FLAG_PHONE_PASSWORD_WRONG;
+        }
+
+        return 0;
+    }
+
+    public static ResultSet getGymBriefAll()
+            throws SQLException
+    {
+        initConnection();
+        Statement statement = connection.createStatement();
+
+        String sentence = "SELECT name, longtitude, latitude, main_image, single_price, vip_price, discount FROM gym_info;";
+        ResultSet resultSet = statement.executeQuery(sentence);
+
+        return resultSet;
+    }
+
+    public static ResultSet getGymBrief(String type)
+            throws SQLException
+    {
+        initConnection();
+        Statement statement = connection.createStatement();
+
+        String sentence = "SELECT name, longtitude, latitude, main_image, single_price, vip_price, discount FROM gym_info WHERE type = " + type + ";";
+        ResultSet resultSet = statement.executeQuery(sentence);
+
+        return resultSet;
+    }
+
+
+    //详细信息是在点进去每个商家之后才请求的，所以详细信息总是只有一条记录
+    public static ResultSet getGymDetail(String gym_id)
+            throws SQLException
+    {
+        initConnection();
+        Statement statement = connection.createStatement();
+
+        String sentence = "SELECT name, longtitude, latitude, main_image, single_price, vip_price, discount, address_city, address_detail, phone, open_time, " +
+                "hardware_info, service_info, star_level FROM gym_info WHERE id = " + gym_id + ";";
+        ResultSet resultSet = statement.executeQuery(sentence);
+
+        return resultSet;
+    }
+
+    //获取每个商家的详情图片
+    public static ResultSet getGymImages(String gym_id)
+            throws SQLException
+    {
+        initConnection();
+        Statement statement = connection.createStatement();
+
+        String sentence = "SELECT path FROM detail_images WHERE gym_id = " + gym_id + ";";
+        ResultSet resultSet = statement.executeQuery(sentence);
+
+        return resultSet;
+    }
+
+    public static float getGymStarLevel(String gym_id)
+            throws SQLException
+    {
+        initConnection();
+        Statement statement = connection.createStatement();
+
+        String sentence = "SELECT AVG(star_level) FROM gym_star_level WHERE gym_id = " + gym_id + ";";
+        ResultSet resultSet = statement.executeQuery(sentence);
+        while (resultSet.next())
+        {
+            return resultSet.getFloat(1);
+        }
+
+        return 0; //如果返回0，表示当前商家还没有人评分
+    }
+
+    public static ResultSet getOrder(String phone, String status)
+            throws SQLException
+    {
+        initConnection();
+        Statement statement = connection.createStatement();
+
+        String sentence = "SELECT user, gym_id, time, status, money FROM order_info WHERE user = " + phone + " AND status = "+ status + ";";
+        ResultSet resultSet = statement.executeQuery(sentence);
+
+        return resultSet;
+    }
+}
